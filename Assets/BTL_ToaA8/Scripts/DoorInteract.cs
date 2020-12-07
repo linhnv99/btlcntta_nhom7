0ecc1aa0196806bdad44b3c645527153ba2cfd3e
PhanDacSon213 <phandacson213@gmail.com>
2020-12-07 23:29:27 +0700

    SonUpdate

---
+++ Assets/BTL_ToaA8/Scripts/DoorInteract.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorInteract : MonoBehaviour
+{
+    Animator m_Animator;
+    private void OnEnable()
+    {
+        m_Animator = this.GetComponent<Animator>();
+        if (!m_Animator.enabled)
+            m_Animator.enabled = true;
+        m_Animator.SetBool("OnOff", false);
+    }
+
+    public void Interact()
+    {
+        bool isOpen = m_Animator.GetBool("OnOff");
+        if (isOpen) m_Animator.SetBool("OnOff", false);
+        else m_Animator.SetBool("OnOff", true);
+    }
+}
