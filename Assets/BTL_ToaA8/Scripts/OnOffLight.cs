8f2933102b1ab44c2d73a241759ee1a8695343b7
PhanDacSon213 <phandacson213@gmail.com>
2020-12-12 21:00:02 +0700

    GhepToaNha

---
+++ Assets/BTL_ToaA8/Scripts/OnOffLight.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnOffLight : MonoBehaviour
+{
+    [SerializeField] GameObject m_Room;
+    [SerializeField] Material green;
+    [SerializeField] Material red;
+
+    public void TurnOnOff()
+    {
+        if(m_Room.activeSelf == true)
+        {
+            m_Room.SetActive(false);
+            this.gameObject.GetComponent<MeshRenderer>().material = red;
+        }
+        else
+        {
+            m_Room.SetActive(true);
+            this.gameObject.GetComponent<MeshRenderer>().material = green;
+        }
+        Debug.Log(m_Room.activeSelf);
+    }
+
+    
+}
